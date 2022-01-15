f4b7a2279bc0ec13360086eb3060e7a98abe4e36
Kamil Salakhov <antdead19@gmail.com>
2022-01-15 14:55:30 +0300

    add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = String(Math.abs(n));
+    let i = 0;
+    let rev = '';
+    while (i < num.length) {
+        rev = num[i] + rev;
+        i = i + 1;
+    }
+    return rev;
+
 }
